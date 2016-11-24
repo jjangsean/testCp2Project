9c8882e3d86f6bfd3492b3ce088ec6ce4a10ffb6
jjangsean <4066898@naver.com>
2016-11-24 16:24:19 +0900

    Frame class implement

---
+++ ConsoleApplication11/ConsoleApplication11/main.cpp
@@ -0,0 +1,17 @@
+#include "Frame.h"
+
+void main() {
+
+	try {
+		Frame *f1 = new Frame;
+		Frame *f2 = new Frame[10];
+
+		delete f1;
+		delete[] f2;
+	}
+	catch (string msg) {
+		cout << "Error msg:" << msg << endl;
+	}
+}
+
+
